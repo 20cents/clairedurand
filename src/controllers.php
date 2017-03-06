93b8e28d3d82d57d1c45e3069bb24cd3d82721dc
Vincent BOURGEOIS <vincent@Mac-mini-de-Vincent.local>
2017-03-06 21:50:24 +0100

    Add caption on image zoom

--- src/controllers.php
+++ src/controllers.php
@@ -13,94 +13,94 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
         'caption' => 'Corset époque 1820',
     ),
     'jeans-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Fabrication de jeans sur commande',
     ),
     'veste-tailleur-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Veste de costume homme - technique tailleur',
     ),
     'sac-noir-turquoise' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Sac à main et pochette - petite série',
     ),
     'chauve-souris-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Vêtement modulable - petite série',
     ),
     'housse-02' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Pochette d\'ordinateur - petite série',
     ),
     'carmen-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Modéliste pour Le Petit Atelier/Les Chorégies d\'Orange 2015<br />Robe chasuble et costume militaire<br/>Réalisation des prototypes, gradation et industrialisation pour la fabrication de 70 pièces en atelier de confection<br /><small>Opéra Carmen - Mise en scène et costumes Louis Désiré</small>',//? petit atelier
         'thumb' => array(
             'className' => 'grid-item--width2'
         ),
     ),
     'foulard-eau-deco' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Foulard - petite série',
     ),
     'sac-gris-parme' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Sac à main et pochette - petite série',
         'thumb' => array(
           'className' => 'grid-item--width2'
         ),
     ),
     'saroual' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Vêtement modulable - petite série',
     ),
     'robe-tieanddye-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Robe sur mesure - création et réalisation',
     ),
     'foulard-eau-ethnique' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Foulard - petite série',
     ),
     'onet-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Lauréate du concours lancé par la société ONET pour la création d\'une nouvelle gamme de vêtements de travail',
     ),
     'onet-02' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Lauréate du concours lancé par la société ONET pour la création d\'une nouvelle gamme de vêtements de travail',
     ),
     'jupe-courte-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Vêtement modulable - petite série',
     ),
     'cosi-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Couturière pour le Festival d\'Aix 2016<br /><small>Opéra Così fan tutte - Mise en scène Christophe Honoré - Costumes Thibault Vancraenenbroeck</small>',
     ),
     'cosi-02' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Couturière pour le Festival d\'Aix 2016<br /><small>Opéra Così fan tutte - Mise en scène Christophe Honoré - Costumes Thibault Vancraenenbroeck</small>',
     ),
     'housse-03' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Pochette d\'ordinateur - petite série',
     ),
     'jupe-courte-02' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Vêtement modulable - petite série',
     ),
     'mitaine' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Mitaines réversibles - petite série',
     ),
     'laine-modulable-orange' => array(
-        'caption' => 'Lorem',
+        'caption' => '"Petite laine modulable"',
     ),
     'tunique-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Vêtement modulable - petite série',
     ),
-    'robe-mariee' => array(
-        'caption' => 'Lorem',
+    'robe-mariee-01' => array(
+        'caption' => 'Robe de mariée sur mesure - création et réalisation',
     ),
     'housse-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Pochette d\'ordinateur - petite série',
         'thumb' => array(
           'className' => 'grid-item--width2'
         ),
     ),
     'tunique-02' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Vêtement modulable - petite série',
     ),
     'sac-noir-marron' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Sac à main et pochette - petite série',
     ),
     'foulard-rayure-fleur' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Foulard - petite série',
     ),
     'alcina-01' => array(
-        'caption' => 'Lorem',
+        'caption' => 'Couturière pour le Festival d\'Aix 2015<br /><small>Opéra Alcina - Mise en scène Katie Mitchell - Costumes Laura Hopkins</small>',
         'thumb' => array(
             'className' => 'grid-item--width2'
         ),
@@ -135,7 +135,7 @@ $app->get('/', function () use ($app, $images) {
         'mitaine',
         'laine-modulable-orange',
         'tunique-01',
-        'robe-mariee',
+        'robe-mariee-01',
         'housse-01',
         'tunique-02',
         'sac-noir-marron',
